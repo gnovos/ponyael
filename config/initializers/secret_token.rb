7d8cd15b453ea2d7e1ad000a98adaefb0df1a0f3
Mason <freemason@gmail.com>
2012-09-02 15:12:17 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ponyael::Application.config.secret_token = '208bccd91de9806c6ee73599cecafcb329600bd05a3714cf6c8010e5d0b6d5a7b4bc75c8134922b12b08f6884d579ee22cf5db83d4f50b74566dce4d3346099f'
